c5648570fa962030572e9476c215aaa64fa78e02
klambo94@gmail.com <klambo94@gmail.com>
2018-12-12 13:15:33 -0600

    Followed Steps from the book to implment a front end.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/Carlist.js'
+import Login from './components/Login.js';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar position="static" color="default">
+          <Toolbar>Carlist</Toolbar>
+        </AppBar>
+        <Login />
       </div>
     );
   }
